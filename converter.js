efce2b2000b2761afe5b2816ce8be42e97deeec7
Sanyyouisf <sanymamdoh@yahoo.com>
2017-02-10 23:24:43 -0600

    finish the first part of temp converter

--- converter.js
+++ converter.js
@@ -1,19 +1,46 @@
-function toCelsius () {
 
+var convertedTemp = "";
+var inputTemp = "";
+
+function toCelsius() {
+	 var F = document.getElementById("Temp").value;
+	 var C = (F -32) * 5 / 9;
+	 convertedTemp = C ;
 }
 
-function toFahrenheit () {
 
+function toFahrenheit() {
+	var C = document.getElementById("Temp").value;
+	var F = (C)* 9 / 5 + 32;
+	convertedTemp = F;
 }
 
-// Get a reference to the button element in the DOM
-var button = document.getElementById("converter");
 
-// This function should determine which conversion should
-// happen based on which radio button is selected.
-function determineConverter (clickEvent) {
-  console.log("event", clickEvent);
-}
+//function for the Reset button 
+document.getElementById("reset").addEventListener("click", function(){
+    document.getElementById("C to F").checked= false;
+	document.getElementById("F to C").checked= false;
+ 	document.getElementById("Temp").value = "";
+ 	document.getElementById("Result").value = "";
+ 	convertedTemp = "";
+ 	 inputTemp = "";
+});
 
-// Assign a function to be executed when the button is clicked
-button.addEventListener("click", determineConverter);
+
+// function to determine which conversion should  when the converter button is checked
+document.getElementById("Converter").addEventListener("click", function(){
+    console.log("converter button is clicked ");
+    if ( document.getElementById("C to F").checked === true){
+    	toFahrenheit();
+    	document.getElementById("Result").value = convertedTemp;
+    }
+    else if ( document.getElementById("F to C").checked === true){
+    	toCelsius();
+    	document.getElementById("Result").value = convertedTemp;
+    } 
+    else {
+    	alert (" please choose F or C first  before converting ");
+    }
+});
+
+
